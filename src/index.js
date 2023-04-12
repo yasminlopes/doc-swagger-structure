dfab8a86d51b31ab1d232f7b459a07ff2020d07b
Yasmin Lopes <yclopes27@gmail.com>
2023-04-12 20:49:12 -0300

    build: initial setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const config = require("./config/config");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./docs/swaggerConfig");
+
+const app = express();
+app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
